3f2b96ce776f881d64bbd21b4f5bf15c6cc555ae
Spdoriano101 <43067378+Spdoriano101@users.noreply.github.com>
2018-09-21 13:56:46 +0100

    Updated Critter - on mouse down critter destroys

---
+++ Assets/Scripts/Critter.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Critter : MonoBehaviour {
+
+    public Vector2 LowerRange;
+    public Vector2 upperRange;
+    
+    
+    // Use this for initialization
+	void Start () {
+
+        transform.position = new Vector3(
+            Random.Range(LowerRange.x, upperRange.x),
+            Random.Range(LowerRange.y, upperRange.y),
+            0
+        );
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+	}
+
+    void OnMouseDown()
+    {
+
+        Destroy(gameObject);
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
